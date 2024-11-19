0ed2980df3c7749f70491b79cefff4309e98b460
ditt <adhytya.prast@gmail.com>
2024-11-20 00:34:22 +0700

    ubah dikit saja

--- caripasangan/src/app/page.tsx
+++ caripasangan/src/app/page.tsx
@@ -37,41 +37,74 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { Tabs, TabsContent, TabsList, TabsTrigger } 
+from "@/components/ui/tabs"
 
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] flex justify-end flex-col gap-x-2 font-[family-name:var(--font-geist-sans)]">
+    <div className="container mx-auto grid grid-rows-[20px_1fr_20px] flex flex-col gap-x-2 font-[family-name:var(--font-geist-sans)]">
       <main className="navbar flex flex-col gap-50 row-start-2 items-center sm:items-end">
         <div className="login">
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
             <Button variant="outline">Open</Button>
           </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-[350px]">
+        <DropdownMenuContent className="w-[410px]">
           <DropdownMenuLabel>Login Please!</DropdownMenuLabel>
             <DropdownMenuSeparator />
-            <Card className="w-[350px]">
-              <CardHeader></CardHeader>
-                  <CardContent>
-                      <form>
-                        <div className="grid w-full items-center gap-4">
-                          <div className="flex flex-col space-y-1.5">
-                            <Label htmlFor="name">Username</Label>
-                            <Input id="name" placeholder="Name of your project" />
-                          </div>
-                          <div className="flex flex-col space-y-1.5">
-                            <Label htmlFor="name">Password</Label>
-                            <Input id="name" placeholder="Name of your project" />
-                          </div>
-                        </div>
-                      </form>
-                  </CardContent>
-                    <CardFooter className="flex justify-between">
-                      <Button variant="outline">Cancel</Button>
-                      <Button>Login</Button>
-                    </CardFooter>
-                  </Card>
+            <Tabs defaultValue="account" className="w-[400px]">
+      <TabsList className="grid w-full grid-cols-2">
+        <TabsTrigger value="account">Login</TabsTrigger>
+        <TabsTrigger value="password">Sign Up</TabsTrigger>
+      </TabsList>
+      <TabsContent value="account">
+        <Card>
+          <CardHeader>
+            <CardTitle>Login to your account</CardTitle>
+            <CardDescription>
+              enjoy aman jaya
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <div className="space-y-1">
+              <Label htmlFor="name">Username</Label>
+              <Input id="name" defaultValue="Username..." />
+            </div>
+            <div className="space-y-1">
+              <Label htmlFor="username">Password</Label>
+              <Input id="username" defaultValue="Password..." />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button>Login</Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+      <TabsContent value="password">
+        <Card>
+          <CardHeader>
+            <CardTitle>Sign Up</CardTitle>
+            <CardDescription>
+              Create your account first.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <div className="space-y-1">
+              <Label htmlFor="current">Username</Label>
+              <Input id="current" type="password" />
+            </div>
+            <div className="space-y-1">
+              <Label htmlFor="new">Password</Label>
+              <Input id="new" type="password" />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button>Sign Up</Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+    </Tabs>
       </DropdownMenuContent>
     </DropdownMenu>
 
