142dcd76b14b21727ce3800fbbe0ecb050d2fdf8
Budi Setyawan <setyawan.5247@widyatama.ac.id>
2024-11-20 14:55:15 +0700

    edit nama website deskripsi dan revisi margin button login

--- uji-coba-next-tailwind/app/page.tsx
+++ uji-coba-next-tailwind/app/page.tsx
@@ -17,7 +17,7 @@ const HomePage = () => {
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">FindUrLover</span>
           </a>
           <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-            <button type="button" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2 me-2 mb-1 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Login</button>
+            <button type="button" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2 me-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Login</button>
 
             <button onClick={handleToggle} type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded={isOpen ? "true" : "false"}>
               <span className="sr-only">Open main menu</span>
