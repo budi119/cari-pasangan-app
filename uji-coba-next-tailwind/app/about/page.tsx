951595eb88a7a6a4581d1308dc2a74a035650584
Budi Setyawan <setyawan.5247@widyatama.ac.id>
2024-11-21 23:05:09 +0700

    menambahkan routing untuk about

---
+++ uji-coba-next-tailwind/app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1 className='mt-20'>About Page</h1>
+        </div>
+    )
+}
+
+export default AboutPage;
