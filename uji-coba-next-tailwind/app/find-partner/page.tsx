d0d86ea1c4dc0f8d8e8edde15bc38f12ac4ea2ca
ditt <adhytya.prast@gmail.com>
2024-11-24 00:43:58 +0700

    add icon web

--- uji-coba-next-tailwind/app/find-partner/page.tsx
+++ uji-coba-next-tailwind/app/find-partner/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-
 const page = () => {
     return (
         <div>
             <h1 className='mt-20'>Halaman Find Partner</h1>
         </div>
+        
     )
 }
 
-export default page
+export default page;
