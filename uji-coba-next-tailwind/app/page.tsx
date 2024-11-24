fbdffd214ce08c90d4d65933d1faae566456c52a
ditt <adhytya.prast@gmail.com>
2024-11-24 19:12:58 +0700

    add highlighted people

--- uji-coba-next-tailwind/app/page.tsx
+++ uji-coba-next-tailwind/app/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import CardUser from './components/CardUser';
+import People from './components/People';
 import {
   Card,
   CardContent,
@@ -63,52 +64,10 @@ const HomePage = () => {
             </div>
             <div className="flow-root">
               <ul role="list" className="divide-y divide-gray-200 dark:divide-gray-700">
-                <li className="py-3 sm:py-4">
-                  <div className="flex items-center">
-                    <div className="flex-shrink-0">
-                      <img className="w-8 h-8 rounded-full object-cover" src="https://cdn.idntimes.com/content-images/duniaku/post/20230203/300587459-644512820424460-4842282226378900658-n-1c2be0cf6d19d21f4eceaf315e8f37ea.jpg" alt="Highlight People" />
-                    </div>
-                    <div className="flex-1 min-w-0 ms-4">
-                      <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
-                        Vonny Felicia
-                      </p>
-                      <p className="text-sm text-gray-500 truncate dark:text-gray-400">
-                        Hobi Memasak dan tertarik dengan pria SIGMA anjayayyyy
-                      </p>
-                    </div>
-                    <div>
-                      <button type="button" className="text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center me-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800 dark:hover:bg-blue-500">
-                        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 576 512">
-                          <path d="M288 80c-65.2 0-118.8 29.6-159.9 67.7C89.6 183.5 63 226 49.4 256c13.6 30 40.2 72.5 78.6 108.3C169.2 402.4 222.8 432 288 432s118.8-29.6 159.9-67.7C486.4 328.5 513 286 526.6 256c-13.6-30-40.2-72.5-78.6-108.3C406.8 109.6 353.2 80 288 80zM95.4 112.6C142.5 68.8 207.2 32 288 32s145.5 36.8 192.6 80.6c46.8 43.5 78.1 95.4 93 131.1c3.3 7.9 3.3 16.7 0 24.6c-14.9 35.7-46.2 87.7-93 131.1C433.5 443.2 368.8 480 288 480s-145.5-36.8-192.6-80.6C48.6 356 17.3 304 2.5 268.3c-3.3-7.9-3.3-16.7 0-24.6C17.3 208 48.6 156 95.4 112.6zM288 336c44.2 0 80-35.8 80-80s-35.8-80-80-80c-.7 0-1.3 0-2 0c1.3 5.1 2 10.5 2 16c0 35.3-28.7 64-64 64c-5.5 0-10.9-.7-16-2c0 .7 0 1.3 0 2c0 44.2 35.8 80 80 80zm0-208a128 128 0 1 1 0 256 128 128 0 1 1 0-256z" />
-                        </svg>
-                        <span className="sr-only">Icon description</span>
-                      </button>
-                    </div>
-                  </div>
-                </li>
-                <li className="py-4 sm:py-4">
-                  <div className="flex items-center">
-                    <div className="flex-shrink-0">
-                      <img className="w-8 h-8 rounded-full object-cover" src="https://th.bing.com/th/id/OIP.5Ww4lig_v4EMipjOv65O2QHaFl?w=214&h=180&c=7&r=0&o=5&pid=1.7" alt="Highlight People" />
-                    </div>
-                    <div className="flex-1 min-w-0 ms-4">
-                      <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
-                        Sheryl Jesslyn
-                      </p>
-                      <p className="text-sm text-gray-500 truncate dark:text-gray-400">
-                        Cewenya kairi accu
-                      </p>
-                    </div>
-                    <div>
-                      <button type="button" className="text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center me-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800 dark:hover:bg-blue-500">
-                        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 576 512">
-                          <path d="M288 80c-65.2 0-118.8 29.6-159.9 67.7C89.6 183.5 63 226 49.4 256c13.6 30 40.2 72.5 78.6 108.3C169.2 402.4 222.8 432 288 432s118.8-29.6 159.9-67.7C486.4 328.5 513 286 526.6 256c-13.6-30-40.2-72.5-78.6-108.3C406.8 109.6 353.2 80 288 80zM95.4 112.6C142.5 68.8 207.2 32 288 32s145.5 36.8 192.6 80.6c46.8 43.5 78.1 95.4 93 131.1c3.3 7.9 3.3 16.7 0 24.6c-14.9 35.7-46.2 87.7-93 131.1C433.5 443.2 368.8 480 288 480s-145.5-36.8-192.6-80.6C48.6 356 17.3 304 2.5 268.3c-3.3-7.9-3.3-16.7 0-24.6C17.3 208 48.6 156 95.4 112.6zM288 336c44.2 0 80-35.8 80-80s-35.8-80-80-80c-.7 0-1.3 0-2 0c1.3 5.1 2 10.5 2 16c0 35.3-28.7 64-64 64c-5.5 0-10.9-.7-16-2c0 .7 0 1.3 0 2c0 44.2 35.8 80 80 80zm0-208a128 128 0 1 1 0 256 128 128 0 1 1 0-256z" />
-                        </svg>
-                        <span className="sr-only">Icon description</span>
-                      </button>
-                    </div>
-                  </div>
-                </li>
+              <People image="/people/vonzy.jpg" name="Vonny Felicia" description="Hobi memasak dan Tertarik dengan pria SIGMA anjayayyayyy"/>
+              <People image="/people/jefri.jpg" name="Jefri Nichol" description="absurdités de l’existence"/>
+              <People image="/people/sze.jpg" name="Sherly Jesslyn" description="Pacarnya Adhyt"/>
+              <People image="/people/dede.jpg" name="Dede Inoen" description="Puncak rantai makanan"/>
               </ul>
             </div>
           </div>
