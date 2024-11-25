9a77a3285f021286cb1b3b26a218f7299cbd3e23
Budi Setyawan <setyawan.5247@widyatama.ac.id>
2024-11-25 19:26:24 +0700

    revisi navbar

--- uji-coba-next-tailwind/app/page.tsx
+++ uji-coba-next-tailwind/app/page.tsx
@@ -1,19 +1,11 @@
 import React from 'react';
 import CardUser from './components/CardUser';
 import People from './components/People';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
 
 
 const HomePage = () => {
   return (
-    <div className='mt-19 bg-[#FADFA1]'>
+    <div className='mt-19'>
       {/* banner utama */}
       <section className="bg-center bg-no-repeat bg-[url('https://plus.unsplash.com/premium_photo-1700353612860-bd8ab8d71f05?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-gray-700 bg-blend-multiply">
         <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
@@ -35,41 +27,22 @@ const HomePage = () => {
       <div className='container grid sm:grid-cols-[4fr_1fr] grid-cols-1 my-auto mx-auto'>
         <div className="flex justify-center items-center mt-10 mb-10">
           <div className='grid sm:grid-cols-4 grid-cols-2 gap-4'>
-<<<<<<< HEAD
-            <CardUser nama="Lora" username="@lauraziphoraa" foto='/talent/lora.jpg' />
-            <CardUser nama="Chelsea" username="@cesiann" foto='/talent/cesian.jpg' />
-            <CardUser nama="Anin" username="@anin.dhitaa" foto='/talent/anin.jpg' />
-            <CardUser nama="Cila" username="@kynchllaalexa" foto='/talent/cila.jpg' />
-            <CardUser nama="Aldean" username="@aldean.tg" foto='/talent/dean.jpg' />
-            <CardUser nama="Kevin" username="@kevinsusanto" foto='/talent/kevin.jpg' />
-            <CardUser nama="L" username="@luthfihalimawan" foto='/talent/L.png' />
-            <CardUser nama="Kairi" username="@kairirds" foto='/talent/kai.jpg' />
-            <CardUser nama="LORA" username="@lauraziphoraa" foto='/talent/lora.jpg' />
-            <CardUser nama="Chelsea" username="@cesiann" foto='/talent/cesian.jpg' />
-            <CardUser nama="Anin" username="@anin.dhitaa" foto='/talent/anin.jpg' />
-            <CardUser nama="Cila" username="@kynchllaalexa" foto='/talent/cila.jpg' />
-            <CardUser nama="Aldean" username="@aldean.tg" foto='/talent/dean.jpg' />
-            <CardUser nama="Kevin" username="@kevinsusanto" foto='/talent/kevin.jpg' />
-            <CardUser nama="L" username="@luthfihalimawan" foto='/talent/L.png' />
-            <CardUser nama="Kairi" username="@kairirds" foto='/talent/kai.jpg' />
-=======
-            <CardUser nama="Lora" username="@lauraziphoraa" foto='/talent/lora.jpg' ig='https://www.instagram.com/lauraziphoraa/' tt='https://www.tiktok.com/@lauraziphoraa'/>
-            <CardUser nama="Chelsea" username="@cesiann" foto='/talent/cesian.jpg' ig='https://www.instagram.com/cesiann/' tt='https://www.tiktok.com/@cesiann'/>
-            <CardUser nama="Anin" username="@anin.dhitaa" foto='/talent/anin.jpg' ig='https://www.instagram.com/aanin.dithaa/' tt='https://www.tiktok.com/@anindthrc'/>
-            <CardUser nama="Cila" username="@kynchllaalexa" foto='/talent/cila.jpg' ig='https://www.instagram.com/kynchllaalexa/' tt='https://www.tiktok.com/@kynchilla'/>
-            <CardUser nama="Aldean" username="@aldean.tg" foto='/talent/dean.jpg' ig='https://www.instagram.com/aldeantg/' tt='https://www.tiktok.com/@aldeantg'/>
-            <CardUser nama="Kevin" username="@kevinsusanto" foto='/talent/kevin.jpg' ig='https://www.instagram.com/xccuratevlr/' tt='https://www.tiktok.com/@xccuratevlr'/>
-            <CardUser nama="L" username="@luthfihalimawan" foto='/talent/L.png' ig='https://www.instagram.com/luthfihalimawan/' tt='https://www.tiktok.com/@luthfi_halimawan'/>
-            <CardUser nama="Kairi" username="@kairirds" foto='/talent/kairi.jpg' ig='https://www.instagram.com/kairirds/' tt='https://www.tiktok.com/@kairirisolmayo'/>
-            <CardUser nama="Lora" username="@lauraziphoraa" foto='/talent/lora.jpg' ig='https://www.instagram.com/lauraziphoraa/' tt='https://www.tiktok.com/@lauraziphoraa'/>
-            <CardUser nama="Chelsea" username="@cesiann" foto='/talent/cesian.jpg' ig='https://www.instagram.com/cesiann/' tt='https://www.tiktok.com/@cesiann'/>
-            <CardUser nama="Anin" username="@anin.dhitaa" foto='/talent/anin.jpg' ig='https://www.instagram.com/aanin.dithaa/' tt='https://www.tiktok.com/@anindthrc'/>
-            <CardUser nama="Cila" username="@kynchllaalexa" foto='/talent/cila.jpg' ig='https://www.instagram.com/kynchllaalexa/' tt='https://www.tiktok.com/@kynchilla'/>
-            <CardUser nama="Aldean" username="@aldean.tg" foto='/talent/dean.jpg' ig='https://www.instagram.com/aldeantg/' tt='https://www.tiktok.com/@aldeantg'/>
-            <CardUser nama="Kevin" username="@kevinsusanto" foto='/talent/kevin.jpg' ig='https://www.instagram.com/xccuratevlr/' tt='https://www.tiktok.com/@xccuratevlr'/>
-            <CardUser nama="L" username="@luthfihalimawan" foto='/talent/L.png' ig='https://www.instagram.com/luthfihalimawan/' tt='https://www.tiktok.com/@luthfi_halimawan'/>
-            <CardUser nama="Kairi" username="@kairirds" foto='/talent/kairi.jpg' ig='https://www.instagram.com/kairirds/' tt='https://www.tiktok.com/@kairirisolmayo'/>
->>>>>>> fbdffd214ce08c90d4d65933d1faae566456c52a
+            <CardUser nama="Lora" username="@lauraziphoraa" foto='/talent/lora.jpg' ig='https://www.instagram.com/lauraziphoraa/' tt='https://www.tiktok.com/@lauraziphoraa' />
+            <CardUser nama="Chelsea" username="@cesiann" foto='/talent/cesian.jpg' ig='https://www.instagram.com/cesiann/' tt='https://www.tiktok.com/@cesiann' />
+            <CardUser nama="Anin" username="@anin.dhitaa" foto='/talent/anin.jpg' ig='https://www.instagram.com/aanin.dithaa/' tt='https://www.tiktok.com/@anindthrc' />
+            <CardUser nama="Cila" username="@kynchllaalexa" foto='/talent/cila.jpg' ig='https://www.instagram.com/kynchllaalexa/' tt='https://www.tiktok.com/@kynchilla' />
+            <CardUser nama="Aldean" username="@aldean.tg" foto='/talent/dean.jpg' ig='https://www.instagram.com/aldeantg/' tt='https://www.tiktok.com/@aldeantg' />
+            <CardUser nama="Kevin" username="@kevinsusanto" foto='/talent/kevin.jpg' ig='https://www.instagram.com/xccuratevlr/' tt='https://www.tiktok.com/@xccuratevlr' />
+            <CardUser nama="L" username="@luthfihalimawan" foto='/talent/L.png' ig='https://www.instagram.com/luthfihalimawan/' tt='https://www.tiktok.com/@luthfi_halimawan' />
+            <CardUser nama="Kairi" username="@kairirds" foto='/talent/kairi.jpg' ig='https://www.instagram.com/kairirds/' tt='https://www.tiktok.com/@kairirisolmayo' />
+            <CardUser nama="Lora" username="@lauraziphoraa" foto='/talent/lora.jpg' ig='https://www.instagram.com/lauraziphoraa/' tt='https://www.tiktok.com/@lauraziphoraa' />
+            <CardUser nama="Chelsea" username="@cesiann" foto='/talent/cesian.jpg' ig='https://www.instagram.com/cesiann/' tt='https://www.tiktok.com/@cesiann' />
+            <CardUser nama="Anin" username="@anin.dhitaa" foto='/talent/anin.jpg' ig='https://www.instagram.com/aanin.dithaa/' tt='https://www.tiktok.com/@anindthrc' />
+            <CardUser nama="Cila" username="@kynchllaalexa" foto='/talent/cila.jpg' ig='https://www.instagram.com/kynchllaalexa/' tt='https://www.tiktok.com/@kynchilla' />
+            <CardUser nama="Aldean" username="@aldean.tg" foto='/talent/dean.jpg' ig='https://www.instagram.com/aldeantg/' tt='https://www.tiktok.com/@aldeantg' />
+            <CardUser nama="Kevin" username="@kevinsusanto" foto='/talent/kevin.jpg' ig='https://www.instagram.com/xccuratevlr/' tt='https://www.tiktok.com/@xccuratevlr' />
+            <CardUser nama="L" username="@luthfihalimawan" foto='/talent/L.png' ig='https://www.instagram.com/luthfihalimawan/' tt='https://www.tiktok.com/@luthfi_halimawan' />
+            <CardUser nama="Kairi" username="@kairirds" foto='/talent/kairi.jpg' ig='https://www.instagram.com/kairirds/' tt='https://www.tiktok.com/@kairirisolmayo' />
           </div>
 
         </div>
@@ -83,10 +56,10 @@ const HomePage = () => {
             </div>
             <div className="flow-root">
               <ul role="list" className="divide-y divide-gray-200 dark:divide-gray-700">
-              <People image="/people/vonzy.jpg" name="Vonny Felicia" description="Hobi memasak dan Tertarik dengan pria SIGMA anjayayyayyy"/>
-              <People image="/people/jefri.jpg" name="Jefri Nichol" description="absurdités de l’existence"/>
-              <People image="/people/sze.jpg" name="Sherly Jesslyn" description="Pacarnya Adhyt"/>
-              <People image="/people/dede.jpg" name="Dede Inoen" description="Puncak rantai makanan"/>
+                <People image="/people/vonzy.jpg" name="Vonny Felicia" description="Hobi memasak dan Tertarik dengan pria SIGMA anjayayyayyy" />
+                <People image="/people/jefri.jpg" name="Jefri Nichol" description="absurdités de l’existence" />
+                <People image="/people/sze.jpg" name="Sherly Jesslyn" description="Pacarnya Adhyt" />
+                <People image="/people/dede.jpg" name="Dede Inoen" description="Puncak rantai makanan" />
               </ul>
             </div>
           </div>
